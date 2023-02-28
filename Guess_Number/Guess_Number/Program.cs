f41d24dfb958ed69ba215c7fff233c03b405accf
kazacvik <kazacvik@cvut.cz>
2023-02-28 15:14:42 +0100

    second

---
+++ Guess_Number/Guess_Number/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Guess_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x, i;
+            string input;
+            Random rnd= new Random();
+            x = rnd.Next(1,100);
+
+            Console.WriteLine("Number is generated");
+
+            while (true)
+            {
+                Console.WriteLine("Guess number:");
+                input = Console.ReadLine();
+                if (input == "end")
+                {
+                    Console.WriteLine("Ending");
+                    break;
+                }
+                else if (!int.TryParse(input, out i)) {
+                    Console.WriteLine("Not a number");
+                    continue;
+                }
+                
+                if (x > i) { Console.WriteLine("Try higher"); }
+                else if (x < i) { Console.WriteLine("Try lower"); }
+                else 
+                {
+                    Console.WriteLine("You win!");
+                    break; 
+                }
+            }
+            
+
+            
+
+        }
+    }
+}
